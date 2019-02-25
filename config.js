72c34ea6b951dc212866b0ed95fdcab3121b56b6
Mark <mark.e.eaton@gmail.com>
2019-02-24 19:23:59 -0600

    added development env vars

--- config.js
+++ config.js
@@ -4,7 +4,14 @@ module.exports = {
   env: process.env.NODE_ENV || 'development',
   development:
    {
-     dbHost: process.env.DB_HOST || '192.168.86.201',
+     dbHost: process.env.DB_HOST_DEV,
+     dbUsername: process.env.DB_USERNAME_DEV,
+     dbPassword: process.env.DB_PASSWORD_DEV,
+     dbDatabaseName: process.env.DB_DATABASE_DEV,
+   },
+   production:
+   {
+     dbHost: process.env.DB_HOST,
      dbUsername: process.env.DB_USERNAME,
      dbPassword: process.env.DB_PASSWORD,
      dbDatabaseName: process.env.DB_DATABASE,
