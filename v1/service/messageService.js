7a397a67d16325c46b745e1262070de1c3b6c0b7
Mark <mark.e.eaton@gmail.com>
2021-02-20 17:44:29 -0600

    commented out message service

--- v1/service/messageService.js
+++ v1/service/messageService.js
@@ -1,34 +1,34 @@
-import config from '../../config';
-import amqp from 'amqplib/callback_api';
+// import config from '../../config';
+// import amqp from 'amqplib/callback_api';
 
-const CONN_URL = config.rabbitMqUrl;
-const EXCHANGE = "TIGER_EXCHANGE";
+// const CONN_URL = config.rabbitMqUrl;
+// const EXCHANGE = "TIGER_EXCHANGE";
 
-let ch = null;
+// let ch = null;
 
-amqp.connect(CONN_URL, (err1, conn) => {
+// amqp.connect(CONN_URL, (err1, conn) => {
 
-   if(err1) {
-      throw err1
-   }
-   console.log('connected to channel');
+//    if(err1) {
+//       throw err1
+//    }
+//    console.log('connected to channel');
 
-   conn.createChannel((err2, channel) => {
-      if (err2) {
-         throw err2;
-      }
-      console.log('created channel');
-      ch = channel;
-      ch.assertExchange(EXCHANGE, 'fanout', {durable: false})
-   });
-});
+//    conn.createChannel((err2, channel) => {
+//       if (err2) {
+//          throw err2;
+//       }
+//       console.log('created channel');
+//       ch = channel;
+//       ch.assertExchange(EXCHANGE, 'fanout', {durable: false})
+//    });
+// });
 
-export const publishToQueue = async (data, queueName = '') => {
-   console.log('published message');
-   ch.publish(EXCHANGE,queueName, Buffer.from(data));
-}
+// export const publishToQueue = async (data, queueName = '') => {
+//    console.log('published message');
+//    ch.publish(EXCHANGE,queueName, Buffer.from(data));
+// }
 
-process.on('exit', (code) => {
-   ch.close();
-   console.log(`Closing rabbitmq channel`);
-});
+// process.on('exit', (code) => {
+//    ch.close();
+//    console.log(`Closing rabbitmq channel`);
+// });
